42dc59f57867df6a49c715a30b45227663011f1c
Alumno <Alumno@PC72>
2024-05-20 11:31:58 -0300

    LISTO LAS OPERACIONES

--- frmBaseDatosOperaciones.Designer.cs
+++ frmBaseDatosOperaciones.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBaseDatosOperaciones));
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvDatosBaseDatos = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -42,42 +42,39 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosBaseDatos)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvDatosBaseDatos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1035, 370);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvDatosBaseDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatosBaseDatos.Location = new System.Drawing.Point(12, 12);
+            this.dgvDatosBaseDatos.Name = "dgvDatosBaseDatos";
+            this.dgvDatosBaseDatos.RowHeadersWidth = 51;
+            this.dgvDatosBaseDatos.Size = new System.Drawing.Size(776, 301);
+            this.dgvDatosBaseDatos.TabIndex = 0;
+            this.dgvDatosBaseDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(16, 393);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 319);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(320, 146);
+            this.groupBox1.Size = new System.Drawing.Size(240, 119);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Operaciones de Proyeccion - SELECT";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(23, 59);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(17, 48);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(272, 28);
+            this.button3.Size = new System.Drawing.Size(204, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Proyeccion multiatributo";
             this.button3.UseVisualStyleBackColor = true;
@@ -85,10 +82,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(23, 95);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(17, 77);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(272, 28);
+            this.button2.Size = new System.Drawing.Size(204, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Juntar";
             this.button2.UseVisualStyleBackColor = true;
@@ -96,10 +92,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 23);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(17, 19);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(272, 28);
+            this.button1.Size = new System.Drawing.Size(204, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Proyecto simple";
             this.button1.UseVisualStyleBackColor = true;
@@ -110,31 +105,28 @@
             this.groupBox2.Controls.Add(this.button4);
             this.groupBox2.Controls.Add(this.button5);
             this.groupBox2.Controls.Add(this.button6);
-            this.groupBox2.Location = new System.Drawing.Point(381, 393);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Location = new System.Drawing.Point(286, 319);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(316, 146);
+            this.groupBox2.Size = new System.Drawing.Size(237, 119);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Operaciones de Seleccion - WHERE";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(23, 59);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(17, 48);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(273, 28);
+            this.button4.Size = new System.Drawing.Size(205, 23);
             this.button4.TabIndex = 2;
             this.button4.Text = "Seleccion multiatributo";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(23, 95);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Location = new System.Drawing.Point(17, 77);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(273, 28);
+            this.button5.Size = new System.Drawing.Size(205, 23);
             this.button5.TabIndex = 1;
             this.button5.Text = "Seleccion por convolucion";
             this.button5.UseVisualStyleBackColor = true;
@@ -142,74 +134,71 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(23, 23);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Location = new System.Drawing.Point(17, 19);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(273, 28);
+            this.button6.Size = new System.Drawing.Size(205, 23);
             this.button6.TabIndex = 0;
             this.button6.Text = "Seleccion simple";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.button7);
             this.groupBox3.Controls.Add(this.button8);
             this.groupBox3.Controls.Add(this.button9);
-            this.groupBox3.Location = new System.Drawing.Point(739, 393);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Location = new System.Drawing.Point(554, 319);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox3.Size = new System.Drawing.Size(312, 146);
+            this.groupBox3.Size = new System.Drawing.Size(234, 119);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "OperacionesAlgebraicas";
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(23, 59);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Location = new System.Drawing.Point(17, 48);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(268, 28);
+            this.button7.Size = new System.Drawing.Size(201, 23);
             this.button7.TabIndex = 2;
             this.button7.Text = "Interseccion";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(23, 95);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Location = new System.Drawing.Point(17, 77);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(268, 28);
+            this.button8.Size = new System.Drawing.Size(201, 23);
             this.button8.TabIndex = 1;
             this.button8.Text = "Diferencia";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(23, 23);
-            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Location = new System.Drawing.Point(17, 19);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(268, 28);
+            this.button9.Size = new System.Drawing.Size(201, 23);
             this.button9.TabIndex = 0;
             this.button9.Text = "Union";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // frmBaseDatosOperaciones
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvDatosBaseDatos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmBaseDatosOperaciones";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmBaseDatosOperaciones";
             this.Load += new System.EventHandler(this.frmBaseDatosOperaciones_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosBaseDatos)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
@@ -219,7 +208,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvDatosBaseDatos;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
--- frmBaseDatosOperaciones.cs
+++ frmBaseDatosOperaciones.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace pryEstructuraDatos
 {
     public partial class frmBaseDatosOperaciones : Form
@@ -16,20 +17,27 @@ namespace pryEstructuraDatos
         {
             InitializeComponent();
         }
-
+        clsBaseDatos objBaseDatos = new clsBaseDatos();
         private void button3_Click(object sender, EventArgs e)
         {
+            //Multiatributo
+            String varSQL = "SELECT TITULO, IDAUTOR, CANTIDAD " + "FROM LIBRO " + "ORDER BY 1 DESC"; 
 
+
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            //JUNTAR
+            String varSQL = "SELECT * " + "FROM LIBRO, AUTOR";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            String varSQL = "SELECT TITULO " + "FROM LIBRO " + "ORDER BY 1 DESC";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
         }
 
         private void frmBaseDatosOperaciones_Load(object sender, EventArgs e)
@@ -39,7 +47,56 @@ namespace pryEstructuraDatos
 
         private void button5_Click(object sender, EventArgs e)
         {
+            //CONVOLUCION
+            String varSQL = "SELECT * " +
+                "FROM (SELECT * FROM LIBRO AS T1 WHERE T1.IDIDIOMA >5) "+
+                "AS T2 WHERE T2.IDAUTOR >10";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
+        }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //SELECCION SIMPLE
+            String varSQL = "SELECT * " + "FROM LIBRO " + "WHERE IDAUTOR = 2";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //SELECCION MULTIATRIBUTO
+            String varSQL = "SELECT * " + "FROM LIBRO " + "WHERE IDAUTOR > 2 AND IDPAIS < 8 ";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //UNUION
+            String varSQL = "SELECT * FROM Libro WHERE IdAutor = 2 "+
+                             "UNION SELECT* FROM Libro WHERE IdAutor = 5 "+
+                             "UNION SELECT* FROM Libro WHERE IdAutor = 3";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            String varSQL = "SELECT * "+
+                            "FROM Libro " +" WHERE IdIdioma "+
+                            "IN(SELECT DISTINCT IdIdioma FROM Libro WHERE IdIdioma < 5)";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //DIFERENCIA
+            String varSQL = "SELECT * " +
+                            "FROM Libro " + " WHERE IdIdioma " +
+                            "NOT IN(SELECT DISTINCT IdIdioma FROM Libro WHERE IdIdioma < 5)";
+            objBaseDatos.Listar(dgvDatosBaseDatos, varSQL);
         }
     }
 }
